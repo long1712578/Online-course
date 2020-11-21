ba4cd052b7f4a2c3679c7fbcd26417c1c33abd46
1712396 <39674758+1712396@users.noreply.github.com>
2020-11-22 03:32:03 +0700

    Add files via upload
    change teacher route

--- Routes/teacher.route.js
+++ Routes/teacher.route.js
@@ -2,9 +2,37 @@ const express=require('express');
 const modelTeacher=require('../Models/teacher.model');
 const router=express.Router();
 
-router.get('/teacher', async(req,res)=>{
+router.get('/', async(req,res)=>{
     const rows=await modelTeacher.getTeacherAll();
-    res.render('users/teacher',{
+    res.render('teacher/index',{
+        teacher:rows,
+        // empty:rows.length===0
+    });
+})
+router.get('/profile', async(req,res)=>{
+    const rows=await modelTeacher.getTeacherAll();
+    res.render('teacher/profile',{
+        teacher:rows,
+        // empty:rows.length===0
+    });
+})
+router.get('/my-course',async(req,res)=>{
+    const rows=await modelTeacher.getTeacherAll();
+    res.render('teacher/my-course',{
+        teacher:rows,
+        // empty:rows.length===0
+    });
+})
+router.get('/chart',async(req,res)=>{
+    const rows=await modelTeacher.getTeacherAll();
+    res.render('teacher/chart',{
+        teacher:rows,
+        // empty:rows.length===0
+    });
+})
+router.get('/help',async(req,res)=>{
+    const rows=await modelTeacher.getTeacherAll();
+    res.render('teacher/help',{
         teacher:rows,
         // empty:rows.length===0
     });
