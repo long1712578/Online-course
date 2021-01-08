5262f3bc8a7c580430485a11836c170185939755
long1712578 <long578@gmail.com>
2021-01-08 13:32:36 +0700

    fix ui admin of huy

--- Routes/admin.route.js
+++ Routes/admin.route.js
@@ -6,7 +6,31 @@ router.get('/dashboad',async (req,res)=>{
     res.render('admin/index',{
         layout:"main_admin",
     });
-})
+});
+
+router.get('/course',async (req,res)=>{
+    res.render('admin/course',{
+        layout:"main_admin",
+    });
+});
+
+router.get('/category',async (req,res)=>{
+    res.render('admin/category',{
+        layout:"main_admin",
+    });
+});
+
+router.get('/chart',async (req,res)=>{
+    res.render('admin/chart',{
+        layout:"main_admin",
+    });
+});
+
+router.get('/teacher',async (req,res)=>{
+    res.render('admin/teacher',{
+        layout:"main_admin",
+    });
+});
 
 
 
